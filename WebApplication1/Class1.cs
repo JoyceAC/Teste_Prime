aa6419bdeb8a99e7f61e1082a0caedc75c7c61f6
jcalandro <jcalandro@HYL-009170>
2023-05-15 12:13:07 -0300

    Add project files.

---
+++ WebApplication1/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+public interface IWarehouseRepository
+{
+    void SetCapacityRecord(int productId, int capacity);
+    IEnumerable<CapacityRecord> GetCapacityRecords();
+    IEnumerable<CapacityRecord> GetCapacityRecords(Func<CapacityRecord, bool> filter);
+
+    void SetProductRecord(int productId, int quantity);
+    IEnumerable<ProductRecord> GetProductRecords();
+    IEnumerable<ProductRecord> GetProductRecords(Func<ProductRecord, bool> filter);
+}
+
+public class CapacityRecord
+{
+    public int ProductId { get; set; }
+    public int Capacity { get; set; }
+}
+
+public class ProductRecord
+{
+    public int ProductId { get; set; }
+    public int Quantity { get; set; }
+}
---
+++ WebApplication1/Controllers/WeatherForecastController.cs
@@ -0,0 +1,172 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Codility.WarehouseApi
+{
+    public class WarehouseController : Controller
+    {
+        private readonly IWarehouseRepository _warehouseRepository;
+
+        public WarehouseController(IWarehouseRepository warehouseRepository)
+        {
+            _warehouseRepository = warehouseRepository;            
+        }        
+
+        // Return OkObjectResult(IEnumerable<WarehouseEntry>)
+        public IActionResult GetProducts()
+        {            
+            List<WarehouseEntry> productList = new List<WarehouseEntry>();            
+
+            foreach (ProductRecord productRecord in _warehouseRepository.GetProductRecords())
+            {
+                if(productRecord.Quantity > 0)
+                {
+                    WarehouseEntry product = new WarehouseEntry();
+                    product.ProductId = productRecord.ProductId;
+                    product.Quantity = productRecord.Quantity;
+
+                    productList.Add(product);
+                }
+            }
+
+            return Ok(productList);
+            // Console.WriteLine("Sample debug output");
+            throw new NotImplementedException();
+        }
+
+        // Return OkResult, BadRequestObjectResult(NotPositiveQuantityMessage), or BadRequestObjectResult(QuantityTooLowMessage)
+        public IActionResult SetProductCapacity(int productId, int capacity, string s)
+        {
+
+            //HttpResponseMessage respMessage = new HttpResponseMessage();
+            Microsoft.AspNetCore.Mvc.ActionResult actionResult = null;
+
+            int qtdeAtual = 0;
+            foreach (ProductRecord productRecord in _warehouseRepository.GetProductRecords())
+            {
+                if (productRecord.ProductId == productId)
+                {
+                    qtdeAtual = productRecord.Quantity;
+                    break;
+                }
+            }
+            if (capacity > 0 && capacity > qtdeAtual)
+            {
+                try
+                {
+                    _warehouseRepository.SetCapacityRecord(productId, capacity);
+                    actionResult = Ok();
+                }
+                
+                catch(Exception ex)
+                {
+                    throw new Exception("Not possible to edit the capacity of the product " + ex);
+                }
+            }
+            else if (capacity <= 0)
+            {
+                NotPositiveQuantityMessage message = new NotPositiveQuantityMessage();
+                actionResult = BadRequest(message);
+                
+            }
+            else
+            {
+                QuantityTooLowMessage message = new QuantityTooLowMessage();
+                actionResult = BadRequest(message); ;
+            }
+
+            return actionResult;
+            
+            //throw new NotImplementedException();
+        }
+
+        // Return OkResult, BadRequestObjectResult(NotPositiveQuantityMessage), or BadRequestObjectResult(QuantityTooHighMessage)
+        public IActionResult ReceiveProduct(int productId, int qty)
+        {
+            Microsoft.AspNetCore.Mvc.ActionResult actionResult = null;
+            int qtdeAtual = 0;
+            foreach (ProductRecord productRecord in _warehouseRepository.GetProductRecords())
+            {
+                if (productRecord.ProductId == productId)
+                {
+                    qtdeAtual = productRecord.Quantity;
+                }
+            }
+            if (qty > 0 && qty <= 999)
+            {
+                foreach (ProductRecord productRecord in _warehouseRepository.GetProductRecords())
+                {
+                    if (productRecord.ProductId == productId)
+                    {
+                        try
+                        {
+                            _warehouseRepository.SetProductRecord(productId, qtdeAtual + qty);
+                            actionResult =  Ok();
+                            break;
+                        }
+                        catch(Exception ex)
+                        {
+                            throw new Exception("Not possible to edit the quantity of the product " + ex);
+                        }
+                    }
+                }
+            }
+            else if(qty <= 0)
+            {
+                NotPositiveQuantityMessage message = new NotPositiveQuantityMessage();
+                actionResult = BadRequest(message);
+            }
+            else
+            {
+                QuantityTooHighMessage message = new QuantityTooHighMessage();
+                actionResult = BadRequest(message);
+            }
+            return actionResult;
+            //throw new NotImplementedException();
+        }
+
+        // Return OkResult, BadRequestObjectResult(NotPositiveQuantityMessage), or BadRequestObjectResult(QuantityTooHighMessage)
+        public IActionResult DispatchProduct(int productId, int qty)
+        {
+            Microsoft.AspNetCore.Mvc.ActionResult actionResult = null;
+            int qtdeAtual = 0;
+            foreach (ProductRecord productRecord in _warehouseRepository.GetProductRecords())
+            {
+                if (productRecord.ProductId == productId)
+                {
+                    qtdeAtual = productRecord.Quantity;
+                }
+            }
+            if (qty > 0 && qty < qtdeAtual)
+            {
+                try
+                {
+                    _warehouseRepository.SetProductRecord(productId, qtdeAtual - qty);
+                    actionResult = Ok();
+                }
+
+                catch (Exception ex)
+                {
+                    throw new Exception("Not possible to edit the capacity of the product " + ex);
+                }
+            }
+            else if (qty <= 0)
+            {
+                NotPositiveQuantityMessage message = new NotPositiveQuantityMessage();
+                actionResult = BadRequest(message);
+            }
+            else
+            {
+                QuantityTooHighMessage message = new QuantityTooHighMessage();
+                actionResult = BadRequest(message);
+            }
+            return actionResult;
+            //throw new NotImplementedException();
+        }
+    }
+}
